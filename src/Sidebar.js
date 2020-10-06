0941c7363acab1096d839a9b3df009c1de5ebc04
awantech-io <hello.wanlab@gmail.com>
2020-10-06 12:27:39 +0800

    sidebar icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,8 +5,21 @@ import SidebarRow from './SidebarRow';
 function Sidebar() {
     return (
         <div className='sidebar'>
-            <SidebarRow title='Pages' />
-            <SidebarRow title='Friends' />
+            <SidebarRow src={user.phoURL} title={user.displayName} />
+
+            <SidebarRow Icon={LocalHospitalIcon} title="COVID-19 Information Center" />
+
+            <SidebarRow Icon={EmojiFlagsIcon} title='Pages' />
+
+            <SidebarRow Icon={PeopleIcon} title='Friends' />
+
+            <SidebarRow Icon={ChatIcon} title='Messenger' />
+
+            <SidebarRow Icon={StorefrontIcon} title='Marketplace' />
+
+            <SidebarRow Icon={VideoLibraryIcon} title='Videos' />
+
+            <SidebarRow Icon={ExpandMoreOutlined} title='Marketplace' />
         </div>
     )
 }
