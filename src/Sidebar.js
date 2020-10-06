62c2a45c46f015eab03cbbf6e5f64a10ced7b7a6
awantech-io <hello.wanlab@gmail.com>
2020-10-06 15:46:04 +0800

    sidebar styles

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import "./Sidebar.css";
 import SidebarRow from './SidebarRow';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags';
+import PeopleIcon from '@material-ui/icons/People';
+import ChatIcon from '@material-ui/icons/Chat';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import {ExpandMoreOutlined} from '@material-ui/icons';
+
+
 
 function Sidebar() {
     return (
         <div className='sidebar'>
-            <SidebarRow src={user.phoURL} title={user.displayName} />
+            {/* <SidebarRow src={user.phoURL} title={user.displayName} /> */}
+
+            <SidebarRow src='https://avatars3.githubusercontent.com/u/59008066?s=460&u=d0a1e975222cbcbea2af851d300cd1fbf0110563&v=4'
+             title='Muhamad Herwan' />
 
             <SidebarRow Icon={LocalHospitalIcon} title="COVID-19 Information Center" />
 
