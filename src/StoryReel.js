37f7dec135bfbefb821747c4dcec7469aa02bbd0
awantech-io <hello.wanlab@gmail.com>
2020-10-08 03:10:09 +0800

    storyreel

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -6,25 +6,25 @@ function StoryReel() {
     return (
         <div className='storyReel'> 
             <Story 
-            image="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/a3liO9nUSCyEhyiAnDOw_Profit-with-JavaScript-new.jpg"
+            image="https://scontent.fkul15-1.fna.fbcdn.net/v/t1.0-9/120948349_3372918106120689_7254529239567879691_n.jpg?_nc_cat=103&_nc_sid=110474&_nc_eui2=AeF-BTWK7cZNuDJd9Xzpc1N4Jj-RLkdOjr4mP5EuR06Ovptxo0Zpud49QJ02iyEnounmXxbA0DYvYkUY08fuOG-q&_nc_ohc=jeZSnMmRLQQAX_ElV8Q&_nc_ht=scontent.fkul15-1.fna&oh=6f28022ec11c68e5c0a97d2e2bba92be&oe=5FA2218D"
             profileSrc="https://avatars3.githubusercontent.com/u/59008066?s=460&u=d0a1e975222cbcbea2af851d300cd1fbf0110563&v=4"
-            title="Muhamad Herwan"
+            title="Your Story"
             />
             <Story 
-            image="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/a3liO9nUSCyEhyiAnDOw_Profit-with-JavaScript-new.jpg"
-            profileSrc="https://avatars3.githubusercontent.com/u/59008066?s=460&u=d0a1e975222cbcbea2af851d300cd1fbf0110563&v=4"
-            title="Muhamad Herwan"
+            image="https://scontent.fkul15-1.fna.fbcdn.net/v/t1.0-9/s960x960/119087278_10218824579951007_6921260000195463281_o.jpg?_nc_cat=101&_nc_sid=dd7718&_nc_eui2=AeGw0KJc48bHrx1wD-5A8ohdz7qNkWu2n93Puo2Ra7af3fMLjzs2xNy-XFUDPMj-ji1IiswK-y4nOWJXF6SfR8hg&_nc_ohc=A6FhIBhKi4kAX-JAhCA&_nc_ht=scontent.fkul15-1.fna&tp=7&oh=a2508aa07130a9f56c27e04d9160d1b5&oe=5FA3A7E2"
+            profileSrc="https://scontent.fkul15-1.fna.fbcdn.net/v/t1.0-1/p240x240/14390790_10207744034384293_1178638006926038679_n.jpg?_nc_cat=104&_nc_sid=dbb9e7&_nc_eui2=AeGrZwJ8fqMlNpN8r6P1YFb0hBXOqCyoqo6EFc6oLKiqjhczqB_UiIzERCOlDdSyPGGRVmh1RWB4z0QJrsD2cnn-&_nc_ohc=guXSIY9KftEAX_0tAk2&_nc_ht=scontent.fkul15-1.fna&tp=6&oh=e96626f115f903e3668a49194eec5213&oe=5FA390F3"
+            title="Hairul Izuwan Bin Hashami"
             />
             <Story 
-            image="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/a3liO9nUSCyEhyiAnDOw_Profit-with-JavaScript-new.jpg"
-            profileSrc="https://avatars3.githubusercontent.com/u/59008066?s=460&u=d0a1e975222cbcbea2af851d300cd1fbf0110563&v=4"
-            title="Muhamad Herwan"
+            image="https://external.fkul15-1.fna.fbcdn.net/safe_image.php?d=AQD7gujF7d9i6NJ5&w=540&h=282&url=https%3A%2F%2Fi.ytimg.com%2Fvi%2FjvOK7nFHgo8%2Fhqdefault.jpg&cfs=1&upscale=1&fallback=news_d_placeholder_publisher&_nc_eui2=AeFXR_BjRg2gMzMY0PCzTdiC8E-3bW1LbtnwT7dtbUtu2bK-U-8Ph26aL_Jb1ERRjTB9BKzP4GH69QfAm5mKoYLn&_nc_cb=1&_nc_hash=AQCJmXB3z4LtwgsG"
+            profileSrc="https://scontent.fkul15-1.fna.fbcdn.net/v/t1.0-1/p240x240/78193911_2790173974347721_3461210471925809152_o.jpg?_nc_cat=106&_nc_sid=dbb9e7&_nc_eui2=AeG48cA_4CauJLbSUERXCTJLVoBmAkLgWptWgGYCQuBam1SZvg0C-4nVVa1AP3iFBJtIeW6tzEZ3rvb2OkRV8t0f&_nc_ohc=C0IwmLMD66UAX_mokeO&_nc_ht=scontent.fkul15-1.fna&tp=6&oh=73b04cc11df3f8a0516a958d02117762&oe=5FA1FB92"
+            title="Muhammad Shakir"
             />
             <Story 
-            image="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/a3liO9nUSCyEhyiAnDOw_Profit-with-JavaScript-new.jpg"
-            profileSrc="https://avatars3.githubusercontent.com/u/59008066?s=460&u=d0a1e975222cbcbea2af851d300cd1fbf0110563&v=4"
-            title="Muhamad Herwan"
-            />
+            image="https://scontent.fkul15-1.fna.fbcdn.net/v/t1.0-9/293517_2214754442360_282605545_n.jpg?_nc_cat=107&_nc_sid=e007fa&_nc_eui2=AeEn7haI4edaIahI8cFhvr42RmCHdE_vYUpGYId0T-9hSrd2rPbgWlLEdJCjqvZvMul01mFqyUPbd9plDF0uuspI&_nc_ohc=zPtNZafvOEoAX8G2HDN&_nc_ht=scontent.fkul15-1.fna&oh=66ff84778476cf79033a735230bc7e6a&oe=5FA3AC91"
+            profileSrc="https://scontent.fkul15-1.fna.fbcdn.net/v/t31.0-1/p240x240/12362945_10206529640333738_6684050146447758811_o.jpg?_nc_cat=106&_nc_sid=dbb9e7&_nc_eui2=AeF31qyqqvxQ481eHQsjqD51mRD4J3hF6oeZEPgneEXqh0BDu_SYIqh2Vlsbhsa-74h4YvpBaIjvvPAPdkw79eGu&_nc_ohc=XrNzGHi-gAoAX9_3psK&_nc_ht=scontent.fkul15-1.fna&tp=6&oh=c52211bbb9d70d6d7a7f439a2d260484&oe=5FA32ABD"
+            title="Hendri Helmi"
+            />  
         </div>
     )
 }
